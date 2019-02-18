253053ccf91d6b026ca138732ad9b2ae4a7323a1
evgenkarp01 <ewal@list.ru>
2019-02-18 21:07:40 +0300

    18022019

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,53 @@
 $( document ).ready(function() {
 
-	
+	$('.gallery-slick').slick({
+	  infinite: true,
+	  dots: true,
+	  arrows: false,
+	  slidesToShow: 6,
+	  slidesToScroll: 5,
+        responsive: [
+            {
+              breakpoint: 1919,
+              settings: {
+                slidesToShow: 6
+              }
+            },
+            {
+              breakpoint: 1600,
+              settings: {
+                slidesToShow: 5
+              }
+            },
+            {
+              breakpoint: 1350,
+              settings: {
+                slidesToShow: 4
+              }
+            },
+            {
+              breakpoint: 1060,
+              settings: {
+                slidesToShow: 3,
+                  slidesToScroll: 3
+              }
+            },
+            {
+              breakpoint: 750,
+              settings: {
+                slidesToShow: 2,
+                  slidesToScroll: 2
+              }
+            },
+            
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+            }
+          ]
+	});
 
 })
